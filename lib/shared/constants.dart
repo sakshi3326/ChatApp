d3f798af666363cc1a93ec03e6bb9d9d7551f751
sakshi3326 <saqkshiiitian123@gmail.com>
2022-12-25 02:00:49 +0530

    basic structure

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:910251550973:web:98a1ace3fe37f26eff7694";
+  static String apiKey = "AIzaSyDQbAcFuaz5e1DTHqw_F_izQQ85PDeh56E";
+  static String messagingSenderId = "910251550973";
+  static String projectId = "chatapp-1b955";
+  final primaryColor = const Color(0xFFee7b64);
+}
